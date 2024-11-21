3d00a459a694f5fdf32c17cfaf6782852c9c0ac3
sumayya-786 <sumayya.m@onmobile.com>
2024-11-21 23:49:15 +0530

    13thNov

---
+++ Nov2024/P5Nov_pythonbasics/class3python-lierals.py
@@ -0,0 +1,25 @@
+num1 = input("Enter number 1")
+num2 = input("Enter number 2")
+num3 = input("Enter number 3")
+num4 = input("Enter number 4")
+
+n1 = int(num1)
+n2 = int(num2)
+n3 = int(num3)
+n4 = int(num4)
+
+add = n1+n2
+sub = n2-n3
+mul = n3*n4
+div = n4/n1
+
+print("addition result : ", add)
+print("subtraction Result : ", sub)
+print("Multiplication result is : ", mul)
+print("Division result is : ", div)
+
+n5 = 15
+n6 = 2
+
+print("remainder is ", n5%n6)
+print("quotient is ", n5/n6)
